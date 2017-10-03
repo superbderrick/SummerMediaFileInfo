52e1fd447e532bae044dca88255108a2f4b14495
derrick <kang.derrick@gmail.com>
2017-10-03 19:23:59 +0900

    added basic project

---
+++ SummerFileInfo/SummerFileInfo.h
@@ -0,0 +1,19 @@
+//
+//  SummerFileInfo.h
+//  SummerFileInfo
+//
+//  Created by Kang Jinyeoung on 03/10/2017.
+//  Copyright © 2017 SuperbDerrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SummerFileInfo.
+FOUNDATION_EXPORT double SummerFileInfoVersionNumber;
+
+//! Project version string for SummerFileInfo.
+FOUNDATION_EXPORT const unsigned char SummerFileInfoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SummerFileInfo/PublicHeader.h>
+
+
