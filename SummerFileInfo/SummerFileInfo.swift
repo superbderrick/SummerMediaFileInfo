a60c8d90dde44dd632dda647397526e07fc41165
derrick <kang.derrick@gmail.com>
2017-10-03 20:04:35 +0900

    added basic files.

---
+++ SummerFileInfo/SummerFileInfo.swift
@@ -0,0 +1,18 @@
+//
+//  SummerFileInfo.swift
+//  SummerFileInfo
+//
+//  Created by Kang Jinyeoung on 03/10/2017.
+//  Copyright © 2017 SuperbDerrick. All rights reserved.
+//
+
+import Foundation
+
+open class SummerFileInfo {
+  var name: ExtractFileType
+  
+  init() {
+    self.name = ExtractFileType.BOTH
+  }
+ 
+}
--- SummerFileInfoDemo/ViewController.swift
+++ SummerFileInfoDemo/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import SummerFileInfo
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //create a SummerFileInfo
   }
 
   override func didReceiveMemoryWarning() {
