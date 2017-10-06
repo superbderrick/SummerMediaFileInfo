787e823195d202cd5b1c885a903355b56c11b5b9
InBeom Pyo <nankkang12@gamail.com>
2017-10-06 18:12:36 +0900

    Demo Code Input

--- featureTest/FeatureTest/FeatureTest/ViewController.swift
+++ featureTest/FeatureTest/FeatureTest/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    var allFiles: PHFetchResult<PHAsset>?
+
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    let allFilesOptions = PHFetchOptions()
+    allFilesOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+    allFilesOptions
+    allFiles = PHAsset.fetchAssets(with: allFilesOptions)
+
     // Do any additional setup after loading the view, typically from a nib.
+    if let files = self.allFiles {
+        for i in 0 ..< files.count {
+            let asset = files[i]
+            let filename = asset.value(forKey: "filename") as! String
+            let fileFormat = asset.value(forKey: "uniformTypeIdentifier") as! String
+            let fileIdentifier = asset.value(forKey: "localIdentifier") as! String
+
+            print("fileName = \(filename)")
+            print("fileFormat = \(fileFormat)")
+            print("filelocal = \(fileIdentifier)")
+            //print("fileType = \(file.mediaType)")
+
+
+        }
+
+    } else {
+        print("Nothing Files")
+    }
+
+    print("Count : \(self.allFiles!.count)")
+
   }
 
   override func didReceiveMemoryWarning() {
